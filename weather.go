8dcee12c74b382550abac4ed78e161d8711777ed
Kyle Suarez <ksuarz@gmail.com>
2014-11-19 23:53:43 -0500

    Update template and add description string
    Weather forecast now takes the weather data and constructs an intelligible
    sentence from all of the different types. It may still need proper insertion of
    commas, though, for when there are multiple weather descriptions at once.

--- weather.go
+++ weather.go
@@ -5,6 +5,7 @@ a REST interface.
 package main
 
 import (
+    "strings"
     "encoding/json"
     "errors"
     "html/template"
@@ -17,6 +18,7 @@ import (
 )
 
 type WeatherDesc struct {
+    Id int `json:"id"`
     Type string `json:"main"`
     Description string `json:"description"`
 }
@@ -38,6 +40,7 @@ type WeatherData struct {
         Pressure float64 `json:"pressure"`
     } `json:"main"`
     Comparison string
+    FullDescription string
 }
 
 type WeatherList struct {
@@ -76,6 +79,60 @@ func getYesterdayFlavorText(hour int) string {
     } else {
         return "yesterday"
     }
+}
+
+func getWeatherDescription(weather WeatherDesc) string {
+    switch weather.Id {
+        case 200, 230: return "thunderstorms with light rain"
+        case 201, 231: return "thunderstorms with rain"
+        case 202, 232: return "thunderstorms with heavy rain"
+        case 210: return "light thunderstorms"
+        case 211: return "thunderstorms"
+        case 212: return "heavy thunderstorms"
+        case 221: return "ragged thunderstorms"
+        case 300, 310: return "light drizzle"
+        case 301, 311: return "drizzling rain"
+        case 302, 312: return "heavy drizzle"
+        case 313, 321: return "showers"
+        case 502, 314, 521: return "heavy rain"
+        case 520, 522: return "light showers"
+        case 531: return "ragged showers"
+        case 620: return "light rain and snow"
+        case 621: return "rain and snow"
+        case 622: return "heavy rain and snow"
+        case 731: return "sand and dust whirls"
+        case 781: return "tornadoes"
+        case 800: return "clear skies"
+        case 801: return "a few clouds"
+        case 803: return "some broken clouds"
+        case 804: return "overcast skies"
+        case 900: return "tornadoes"
+        case 901: return "tropical storms"
+        case 902, 962: return "hurricane conditions"
+        case 903: return "extreme cold"
+        case 904: return "extreme heat"
+        case 905: return "extreme winds"
+        case 906: return "extreme hail"
+        case 951: return "calm weather"
+        case 952: return "light breezes"
+        case 953: return "gentle breezes"
+        case 954: return "moderate breezes"
+        case 955: return "fresh breezes"
+        case 956: return "strong breezes"
+        case 958: return "windy, gale-like conditions"
+        case 959: return "severe gales"
+        case 960: return "storms"
+        case 961: return "violent storms"
+        default: return weather.Description
+    }
+}
+
+func getFullWeatherDescription(weather []WeatherDesc) string {
+    var descs []string = make([]string, len(weather))
+    for i := 0; i < len(weather); i = i + 1 {
+        descs[i] = getWeatherDescription(weather[i])
+    }
+    return strings.Join(descs, ", ")
 }
 
 func renderTemplate(w http.ResponseWriter, tmpl string, data interface{}) {
@@ -138,9 +195,9 @@ func handleWeather(w http.ResponseWriter, r *http.Request) {
 
     // Data sanitization
     var datum WeatherData = data.List[0]
-    //data.List[0].Main.Temperature = math.Floor(data.List[0].Main.Temperature+0.5)
     datum.Comparison = getComparison(datum.Main.Temperature, city)
     datum.Main.Temperature = math.Floor(datum.Main.Temperature + 0.5)
+    datum.FullDescription = getFullWeatherDescription(datum.Weather)
 
     // Render a template
     renderTemplate(w, "weather", datum)
@@ -172,9 +229,12 @@ func getComparison(today float64, city string) string {
 
     // Unmarshal
     err = json.Unmarshal(buf, &data)
-    if err != nil || len(data.List) == 0 {
+    if err != nil {
         log.Printf("Couldn't get yesterday's data - unmarshaling failed.")
         log.Printf("%v", err)
+        return ""
+    } else if len(data.List) == 0 {
+        log.Printf("API response found no data for yesterday :(")
         return ""
     }
 
@@ -184,17 +244,17 @@ func getComparison(today float64, city string) string {
     var diff float64 = today - yesterday
 
     log.Printf("Detected temperature difference from yesterday: %f", diff)
-    if diff < -10.0 {
+    if diff < -5 {
         return tft + " is much colder than " + yft + "."
-    } else if diff < -5.0 {
+    } else if diff < -2.5 {
         return tft + " is colder than " + yft + "."
     } else if diff < -1.0 {
         return tft + " is cooler than " + yft + "."
     } else if diff < 1.0 {
         return tft + "'s temperature is similar to " + yft + "."
-    } else if diff < 5.0 {
+    } else if diff < 2.5 {
         return tft + " is warmer than " + yft + "."
-    } else if diff < 10.0 {
+    } else if diff < 5.0 {
         return tft + " is hotter than " + yft + "."
     } else {
         return tft + " is much hotter than " + yft + "."
